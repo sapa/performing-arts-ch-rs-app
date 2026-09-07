c1c444140bcf6edbfa12245c0a55a3fcc072c98f
Florian Kräutli <florian.kraeutli@uzh.ch>
2026-09-07 09:15:13 +0200

    change pdf viewer version

--- assets/pdfjs-4.2.67-dist/build/pdf.js
+++ assets/pdfjs-4.2.67-dist/build/pdf.js
@@ -3686,7 +3686,7 @@ const PDFWorkerUtil = {
 {
   if (_shared_util_js__WEBPACK_IMPORTED_MODULE_0__.isNodeJS) {
     PDFWorkerUtil.isWorkerDisabled = true;
-    _worker_options_js__WEBPACK_IMPORTED_MODULE_14__.GlobalWorkerOptions.workerSrc ||= "./pdf.worker.mjs";
+    _worker_options_js__WEBPACK_IMPORTED_MODULE_14__.GlobalWorkerOptions.workerSrc ||= "./pdf.worker.js";
   }
   PDFWorkerUtil.isSameOrigin = function (baseUrl, otherUrl) {
     let base;
